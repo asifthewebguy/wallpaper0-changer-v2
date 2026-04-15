5b400ca8b345e3192d3d5f36ee8c8c1b1546cec9
[Mohammad Asif Ullah Chowdhury] <[asifthewebguy@gmail.com]>
2026-04-15 16:37:34 +0600

    fix: valid main.dart stub, gitkeep for assets, gitignore flutter artifact

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MaterialApp(home: Scaffold(body: Center(child: Text('Wallpaper Changer')))));
+}
