9b23ecc8ff744265df968aee13ec7be2dde8b019
[Mohammad Asif Ullah Chowdhury] <[asifthewebguy@gmail.com]>
2026-04-16 13:52:39 +0600

    test: assert MacosProtocolRegistrar implements ProtocolRegistrar

--- test/platform/macos_protocol_registrar_test.dart
+++ test/platform/macos_protocol_registrar_test.dart
@@ -1,9 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:wallpaper_changer/platform/macos_protocol_registrar.dart';
+import 'package:wallpaper_changer/platform/protocol_registrar.dart';
 
 void main() {
   test('register completes without error', () async {
     final registrar = MacosProtocolRegistrar();
     await expectLater(registrar.register(), completes);
+  });
+
+  test('MacosProtocolRegistrar implements ProtocolRegistrar', () {
+    expect(MacosProtocolRegistrar(), isA<ProtocolRegistrar>());
   });
 }
